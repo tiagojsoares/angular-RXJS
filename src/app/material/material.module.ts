5356648369e6a2faf40bb1b48a0254f19c1587ec
tiagojsoares <tiagoedah@live.com>
2020-04-04 19:54:54 -0300

    1 file modified, 1 added

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,110 @@
+import { NgModule } from "@angular/core";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { RouterModule } from "@angular/router";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { HttpClientModule } from "@angular/common/http";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
+/// Import forms
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatListModule } from "@angular/material/list";
+
+
+// Date table
+import { MatTableModule } from "@angular/material/table";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatNativeDateModule, MatRippleModule } from "@angular/material/core";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSortModule } from "@angular/material/sort";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatTreeModule } from "@angular/material/tree";
+//primeNg
+
+@NgModule({
+  imports: [
+    MatInputModule,
+    MatSelectModule,
+    HttpClientModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatBadgeModule,
+    MatTableModule,
+    MatMenuModule,
+    MatTooltipModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    RouterModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatSnackBarModule,    
+    MatAutocompleteModule,
+    MatBottomSheetModule,
+    MatFormFieldModule,
+    MatButtonToggleModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,   
+    MatRippleModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatRadioModule
+    
+  
+    
+  ],
+  exports: [],
+  declarations: []
+})
+export class MaterialModule {}
+
+
+
+
+
+
+
